7d0e32c9fb5d88983b7f13edf4319b051484c1f6
yanghan234 <yanghan234@outlook.com>
2018-08-29 11:11:18 -0500

    Add two source files. 1) pass variables by their references; 2) static memory allocation.

---
+++ section--2/by_reference.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> sort(vector<int> arr, unsigned by=0);
+void swap_by_value(int a, int b);
+void swap_by_reference(int &a, int &b);
+void swap_by_pointer(int *a, int *b);
+void output_results(vector<int> a, vector<int> b);
+
+int main()
+{
+    const int size = 8;
+    vector<int> arr(size),arr0(size),arr1(size),arr2(size);
+    arr = {1,3,2,4,5,7,6,8};
+
+    arr0 = sort(arr,0);
+    output_results(arr,arr0);
+    arr1 = sort(arr,1);
+    output_results(arr,arr1);
+    arr2 = sort(arr,2);
+    output_results(arr,arr2);
+}
+
+vector<int> sort(vector<int> arr, unsigned by)
+{
+    if (by == 0){
+        cout << "Sorting by value" << endl;
+    } else if (by == 1){
+        cout << "Sorting by reference" << endl;
+    } else if (by == 2){
+        cout << "Sorting by pointer" << endl;
+    }
+    for (unsigned i = 0; i < arr.size()-1; i++){
+        for (unsigned j = i+1; j < arr.size(); j++){
+            if (arr[i] > arr[j]){
+                if ( by == 0 ){
+                    swap_by_value(arr[i],arr[j]);
+                }else if ( by == 1 ){
+                    swap_by_reference(arr[i],arr[j]);
+                }else if ( by == 2 ){
+                    swap_by_pointer(&arr[i],&arr[j]);
+                }
+            }
+        }
+    }
+    return arr;
+}
+
+void swap_by_value(int a, int b)
+{
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void swap_by_reference(int &a, int &b)
+{
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void swap_by_pointer(int *a, int *b)
+{
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void output_results(vector<int> a, vector<int> b)
+{
+    cout << "Before sorting: ";
+    for (unsigned i=0; i<a.size(); i++) cout << a[i] << " ";
+    cout << endl;
+    cout << "After sorting:  ";
+    for (unsigned i=0; i<b.size(); i++) cout << b[i] << " ";
+    cout << endl;
+}
---
+++ section--2/static_mem.cpp
@@ -0,0 +1,40 @@
+/* 
+ * Han Yang, Aug-29-2018
+ * static memory
+ * As an example, we compute first several elements in Fibonacci sequence.
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void fib_seq(unsigned size);
+
+int main()
+{
+    fib_seq(5);
+    fib_seq(10);
+    fib_seq(8);
+}
+
+void fib_seq(unsigned size)
+{
+    static vector<int> FibArr;
+    if (size < FibArr.size()){
+        cout << "Already calculated!" << endl;
+    }else{
+        cout << "Some elements need to be computed!" << endl;
+        for (unsigned i=FibArr.size(); i<size; i++){
+            if (i == 0 or i == 1) {
+                FibArr.push_back(1);
+            }else{
+                FibArr.push_back(FibArr[i-1]+FibArr[i-2]);
+            }
+        }
+    }
+    cout << "Fibonacci sequence is computed: " << endl;
+    for (unsigned i=0; i<size; i++){
+        cout << FibArr[i] << " ";
+    }
+    cout << endl;
+}
+
