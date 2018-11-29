51c296307e96ae762582b10e581bc22dfcda706f
Han Yang <yanghan234@outlook.com>
2018-11-29 14:49:50 -0600

    BinaryTree_v2 insert, search, displayTree work well. However, the code to remove node in the tree still has bugs and need to be fixed later.

--- chap6/BinaryTree_v2.cpp
+++ chap6/BinaryTree_v2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
 
@@ -17,7 +18,9 @@ class Node
 
         valType val() const { return _val; };
         unsigned cnt() const { return _cnt; };
-        void display_me( const Node<valType> * );
+
+        Node<valType>* left() const { return _left; };
+        Node<valType>* right() const { return _right; };
 
     private:
         valType _val;
@@ -31,10 +34,13 @@ class BinaryTree
 {
     public:
         BinaryTree(){ _root = 0; };
+        Node<valType>* root() const { return _root; };
 
         void insert( const valType &elem );
+        bool remove( const valType &elem );
+        bool search( const valType &elem );
 
-        void display_tree( );
+        void displayTree( );
 
     private:
         Node<valType> *_root;
@@ -49,22 +55,14 @@ Node<valType>::Node( const valType &elem ) : _val(elem)
 }
 
 template < typename valType >
-void Node<valType>::display_me( const Node<valType> *ptr )
-{
-    if ( !ptr ) return; // if ptr is empty, do nothing
-    if ( ptr->_left )
-
-}
-
-template < typename valType >
 ostream& operator<<( ostream &os, const Node<valType> *ptr )
 {
     if ( !ptr ) return os; // if ptr is empty, do nothing
-    if ( ptr->_left )
-      os << ptr->_left;
+    if ( ptr->left() )
+      os << ptr->left();
     os << "{ val: " << ptr->val() << ", cnt: " << ptr->cnt() << " }" << endl;
-    if ( ptr->_right )
-      os << ptr->_right;
+    if ( ptr->right() )
+      os << ptr->right();
     return os;
 }
 
@@ -89,36 +87,204 @@ void BinaryTree<valType>::insert( const valType &elem )
             }
             else if ( p->_val > elem )
             {
-                p = p->_left;
+                if ( !(p->_left) )
+                {
+                    p->_left = new Node<valType>(elem);
+                    return;
+                }
+                else
+                    p = p->_left;
             }
             else
             {
-                p = p->_right;
+                if ( !(p->_right) )
+                {
+                    p->_right = new Node<valType>(elem);
+                    return;
+                }
+                else
+                    p = p->_right;
             }
         }
-        p = new Node<valType>(elem);
-        return;
     }
 }
 
 template < typename valType >
-void BinaryTree<valType>::display_tree( )
+bool BinaryTree<valType>::remove( const valType &elem )
+{
+    /* return value:
+     * 1: empty tree or the elem is not in the tree
+     * 0: elem is in the tree and has been removed gracefully
+     */
+    if ( !_root )
+        // empty tree
+        return false;
+    else
+    {
+        Node<valType> *ptr = _root;
+        Node<valType> *parent = _root;
+        while ( ptr )
+        {
+            if ( ptr->_val > elem )
+            {   // _val > elem, find elem in left subtree
+                if ( !(ptr->_left) )
+                    return true;
+                else
+                {
+                    if ( ptr == _root )
+                        ptr = ptr->_left;
+                    else
+                    {
+                        parent = ptr;
+                        ptr = ptr->_left;
+                    }
+                }
+            }
+            else if ( ptr->_val < elem )
+            {   // _val < elem, find elem in right subtree
+                if ( !(ptr->_right) )
+                    return true;
+                else
+                {
+                    if ( ptr == _root )
+                      ptr = ptr->_right;
+                    else
+                    {
+                      parent = ptr;
+                      ptr = ptr->_right;
+                    }
+                }
+
+            }
+            else
+              break;
+        } // end of while
+
+        /* Now, we have found the target node and its parent.
+         * To remove it, we substitute the tartget node
+         * with the smallest value in the right subtree.
+         */
+
+        if ( ptr == parent )
+        {   // ptr = parent = root, i.e. to remove root node
+            if ( !(ptr->_right) )
+            {
+                _root = ptr->_left;
+                delete ptr;
+            }
+            else
+            {
+                Node<valType> *ptr2 = ptr->_right;
+                Node<valType> *parent2 = ptr->_right;
+                while ( ptr2->_left )
+                {
+                    if ( ptr2 == ptr->_right )
+                        ptr2 = ptr2->_left;
+                    else
+                    {
+                        parent2 = ptr2;
+                        ptr2 = ptr2->_left;
+                    }
+                }
+                ptr->_val = ptr2->_val;
+                ptr->_cnt = ptr2->_cnt;
+                parent2->_left = 0;
+                delete ptr2;
+            }
+        }
+        else
+        {
+            if ( !(ptr->_right) )
+            {
+                if ( parent->_left == ptr )
+                    parent->_left = ptr->_left;
+                else
+                    parent->_right = ptr->_left;
+                delete ptr;
+            }
+            else
+            {
+                Node<valType> *ptr2 = ptr->_right;
+                Node<valType> *parent2 = ptr->_right;
+                while ( ptr2->_left )
+                {
+                    if ( ptr2 == ptr->_right )
+                        ptr2 = ptr2->_left;
+                    else
+                    {
+                        parent2 = ptr2;
+                        ptr2 = ptr2->_left;
+                    }
+                }
+                ptr->_val = ptr2->_val;
+                ptr->_cnt = ptr2->_cnt;
+                parent2->_left = 0;
+                delete ptr2;
+            }
+        } // endif
+        return true;
+    }
+}
+
+template < typename valType >
+bool BinaryTree<valType>::search( const valType &elem )
+{
+    if ( !_root )
+        return false;
+    else
+    {
+        Node<valType> *ptr = _root;
+        while ( ptr )
+        {
+            if ( ptr->_val > elem )
+            {
+                if ( !(ptr->_left) )
+                    return false;
+                else
+                    ptr = ptr->_left;
+            }
+            else if ( ptr->_val < elem )
+            {
+                if ( !(ptr->_right) )
+                    return false;
+                else
+                    ptr = ptr->_right;
+            }
+            else
+              return true;
+        } // end of while
+    } // end of if the tree is not empty
+}
+
+template < typename valType >
+void BinaryTree<valType>::displayTree( )
 {
     if ( !_root )
     {
         cout << "The tree is empty." << endl;
         return;
     }
-    Node<valType> *ptr = _root;
-    
-    cout << ptr;
+    cout << _root << endl;
 }
 
 int main()
 {
+    // 顺便用二叉树实现了一遍排序
+    int arr[10] = {9,45,485,15,567,349,167,02,12,41};
     BinaryTree<int> bt;
-    bt.insert(1);
-    bt.insert(0);
-    bt.insert(2);
-    bt.display_tree();
+    for ( int i = 0; i < 10; i++ )
+    {
+      bt.insert(arr[i]);
+    }
+    bt.displayTree();
+
+    ofstream outFile("output.dat");
+    outFile << bt.root();
+    outFile.close();
+
+    bt.remove(9);
+    bt.remove(12);
+
+    bt.displayTree();
+
 }
